0ae94a805483a6f03aeef3883f0578c8efcb3141
yichien <kevinyichienlee@gmail.com>
2020-12-17 22:50:07 +0800

    property decorator

---
+++ CS200/property.py
@@ -0,0 +1,28 @@
+# @property
+
+# encapsulation 封裝
+
+class Batman:
+    def __init__(self,hp):
+        self.hp = hp #property
+
+    @property
+    def hp(self):
+        print("asdasd")
+        return self._hp
+    
+    @hp.setter
+    def hp(self,hp):
+        if hp > 100:
+            self._hp = 100
+        elif hp < 0:
+            self._hp = 0
+        else:
+            self._hp = hp
+
+            
+b1 =Batman(100)
+
+b1.hp = 300
+
+print(b1.hp)
