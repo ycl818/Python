27d7b6062b950975f9de80203bcb9a031683d557
yichien <kevinyichienlee@gmail.com>
2020-12-19 00:00:59 +0800

    comment style

---
+++ CS200/comment.py
@@ -0,0 +1,46 @@
+"""A tool to .....
+
+A longer description
+...
+...
+...
+
+
+"""
+
+
+class Batman:
+    """apppppppppppppppppppppppp
+
+    """
+    pass
+
+print(Batman.__doc__)
+
+# coding style
+
+# x =5  # this is a comment
+
+# asdf
+# asdfas
+# fasfasdfasdfasdf
+# asdfsdfasdfasdf
+
+
+
+"""
+asdasd
+asdasd
+asdasd
+
+"""
+
+'''
+asdasd
+asdasd
+
+'''
+
+# docstring 
+
+# doc documentation
